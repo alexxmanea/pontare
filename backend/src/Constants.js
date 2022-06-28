a68c99940923e5160e89c676334eadcaef7ad120
Alexandru Manea <alexg.manea@gmail.com>
2022-06-28 21:30:34 +0300

    Initial commit
    Added a .gitignore
    Added a working backend that successfully mark a working day in the
    time sheet application, using puppeteer.
    Started the implementation for the configuration app (frontend).

---
+++ backend/src/Constants.js
@@ -0,0 +1,35 @@
+export const LOGIN_URL = "https://www.e-trans.ro/prlogs-beta/login.php";
+
+export const FIELDS = {
+    login: {
+        username:
+            "#inner-container > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > input[type=text]",
+        password:
+            "#inner-container > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=password]",
+        submit: "#inner-container > form > table > tbody > tr:nth-child(3) > td:nth-child(1) > input[type=submit]",
+        errorText:
+            "Nu sunteti autorizat sa folositi sistemul. Va rugam sa va autentificati!",
+    },
+    manopera: {
+        main: "#navigation_buyerpg > p > span:nth-child(7) > a",
+        add: "#subnav_txt > a:nth-child(1)",
+        project: {
+            work: {
+                select: "#fk_proiect",
+                option: "#fk_proiect > option:nth-child(13)",
+            },
+            vacation: {
+                select: "#fk_proiect",
+                option: "#fk_proiect > option:nth-child(5)",
+            },
+        },
+        date: "#data_efectuare",
+        hours: { element: "#nr_ore", value: "8" },
+        isWorkFromHome: "#is_telemunca",
+        submit: "#submit_btn",
+    },
+};
+
+export const DATE_FORMAT = "YYYY-MM-DD";
+
+export const DAY_TYPES = { work: 0, vacation: 1 };
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import Login from "./components/login/Login";
+import "@fontsource/roboto";
+
+function App() {
+    return <Login />;
+}
+
+export default App;
